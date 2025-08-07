c0c0f20e718a819e5b139284458d0eb1b4309fb2
Nathan <70660308+NotARoomba@users.noreply.github.com>
2025-08-06 21:47:01 -0500

    Update test.py

--- firmware/test.py
+++ firmware/test.py
@@ -6,8 +6,9 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        id, text = reader.read()
-        print(id)
-        print(text)
+        text = input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
 finally:
         GPIO.cleanup()
