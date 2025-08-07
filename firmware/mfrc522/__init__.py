4ba3c9641399067e50c3f8e06e3fa374acb55754
Nathan <70660308+NotARoomba@users.noreply.github.com>
2025-08-06 23:48:41 -0500

    aaa

--- firmware/mfrc522/__init__.py
+++ firmware/mfrc522/__init__.py
@@ -1,4 +1,8 @@
-from .MFRC522 import MFRC522
+# Import the main classes and enums
+from .MFRC522 import MFRC522, NTAGType
 from .SimpleMFRC522 import SimpleMFRC522
+
+# Make sure NTAGType is available at module level
+__all__ = ['MFRC522', 'NTAGType', 'SimpleMFRC522']
 
 name = "mfrc522"
